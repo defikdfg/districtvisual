e5c0940d3d3109cff445d444e69b8d82fbaafdb7
defikdfg <venor5665@gmail.com>
2026-05-31 00:37:16 +1100

    Update InGameHudMixin.java

--- src/main/java/com/district/visual/mixin/InGameHudMixin.java
+++ src/main/java/com/district/visual/mixin/InGameHudMixin.java
@@ -1,8 +1,9 @@
 package com.district.visual.mixin;
 
 import com.district.visual.DistrictVisualClient;
-import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,9 +13,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class InGameHudMixin {
 
     @Inject(method = "render", at = @At("TAIL"))
-    private void onRender(DrawContext ctx, float tickDelta, CallbackInfo ci) {
+    private void onRender(DrawContext ctx, RenderTickCounter counter, CallbackInfo ci) {
         if (DistrictVisualClient.getInstance() != null) {
-            DistrictVisualClient.getInstance().getModuleManager().onRenderHud(ctx, tickDelta);
+            DistrictVisualClient.getInstance().getModuleManager().onRenderHud(ctx, counter.getTickDelta(true));
         }
     }
 }
